e32da20411bf2eca36464b91890d8161d5cf9356
Aidan Nguyen <aidannguyen721@gmail.com>
2024-07-28 21:58:39 -0400

    Installed dependencies for webscraping

---
+++ GT Crime Logs/Pods/Target Support Files/Pods-GT_Crime_Logs-GT_Crime_LogsUITests/Pods-GT_Crime_Logs-GT_Crime_LogsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GT_Crime_Logs_GT_Crime_LogsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GT_Crime_Logs_GT_Crime_LogsUITests
+@end
---
+++ GT Crime Logs/Pods/Target Support Files/Pods-GT_Crime_LogsTests/Pods-GT_Crime_LogsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GT_Crime_LogsTests : NSObject
+@end
+@implementation PodsDummy_Pods_GT_Crime_LogsTests
+@end
